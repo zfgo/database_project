ac0727411d2cd7a6c38e30417f200b98fb12dab1
Zane Globus-O'Harra <zanegloh@gmail.com>
2023-03-15 16:08:50 -0700

    typo

--- driver_avg_season.php
+++ driver_avg_season.php
@@ -29,7 +29,7 @@ $season = mysqli_real_escape_string($conn, $season);
 // this is a small attempt to avoid SQL injection
 // better to use prepared statements
 
-$query = "SELECT CONCAT(d.fname, " ", d.lname) AS driver_name, AVG(r.points_position) AS avg_position, AVG(p.points) AS avg_points
+$query = "SELECT CONCAT(d.fname, ' ', d.lname) AS driver_name, AVG(r.points_position) AS avg_position, AVG(p.points) AS avg_points
           FROM f1db.result r JOIN f1db.race ra ON r.race_race_id = ra.race_id AND r.race_season_year = ra.season_year
               JOIN f1db.driver d ON r.driver_driver_id = d.driver_id
               JOIN f1db.points p ON r.points_position = p.position
@@ -56,16 +56,16 @@ Result of query:
 $result = mysqli_query($conn, $query)
 or die(mysqli_error($conn));
 
-$mask = "| %-18s | %10s | %10s |\n";
+$mask = "| %-18s | %12s | %10s |\n";
 print "<pre>";
-printf($mask, "------------------", "----------", "----------");
+printf($mask, "------------------", "------------", "----------");
 printf($mask, "driver name", "avg position", "avg points");
-printf($mask, "------------------", "----------", "----------");
+printf($mask, "------------------", "------------", "----------");
 while($row = mysqli_fetch_array($result, MYSQLI_BOTH))
 {
-    printf($mask, "$row[driver_name]", "$row[avg_points]", "$row[avg_position]");
+    printf($mask, "$row[driver_name]", "$row[avg_position]", "$row[avg_points]");
 }
-printf($mask, "------------------", "----------", "----------");
+printf($mask, "------------------", "------------", "----------");
 print "</pre>";
 
 mysqli_free_result($result);
