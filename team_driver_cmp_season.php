12f40a071650517b4c47a07f20aeaca82908d748
Zane <zanegloh@gmail.com>
2023-03-15 20:17:07 -0700

    add driver age

--- team_driver_cmp_season.php
+++ team_driver_cmp_season.php
@@ -31,14 +31,20 @@ $team = mysqli_real_escape_string($conn, $team);
 // this is a small attempt to avoid SQL injection
 // better to use prepared statements
 
-$query = "SELECT CONCAT(d.fname, ' ', d.lname) as driver_name, d.code AS driver_code, d.number AS driver_num, SUM(p.points) AS total_points, COUNT(r.race_race_id) AS num_races
+// $query = "SELECT CONCAT(d.fname, ' ', d.lname) as driver_name, d.code AS driver_code, d.number AS driver_num, SUM(p.points) AS total_points, COUNT(r.race_race_id) AS num_races
+//           FROM f1db.result r JOIN f1db.driver d ON r.driver_driver_id = d.driver_id
+//               JOIN f1db.team t ON d.team_team_id = t.team_id
+//               JOIN f1db.points p ON r.points_position = p.position
+//           WHERE t.name LIKE '".$team."' AND r.race_season_year = ".$season."
+//           GROUP BY d.driver_id, d.fname, d.lname, d.code, d.number
+//           ORDER BY total_points DESC;";
+$query = "SELECT CONCAT(d.fname, ' ', d.lname) as driver_name, d.code AS driver_code, d.number AS driver_num, YEAR(CURDATE())-YEAR(d.dob) AS driver_age, SUM(p.points) AS total_points, COUNT(r.race_race_id) AS num_races
           FROM f1db.result r JOIN f1db.driver d ON r.driver_driver_id = d.driver_id
               JOIN f1db.team t ON d.team_team_id = t.team_id
               JOIN f1db.points p ON r.points_position = p.position
           WHERE t.name LIKE '".$team."' AND r.race_season_year = ".$season."
           GROUP BY d.driver_id, d.fname, d.lname, d.code, d.number
           ORDER BY total_points DESC;";
-
 ?>
 
 <p>
@@ -58,16 +64,16 @@ Result of query:
 $result = mysqli_query($conn, $query)
 or die(mysqli_error($conn));
 
-$mask = "| %-18s | %-12s | %-10s | %12s | %9s |\n";
+$mask = "| %-18s | %-12s | %-10s | %4s | %12s | %9s |\n";
 print "<pre>";
-printf($mask, "------------------", "------------", "----------", "------------", "---------");
-printf($mask, "driver name", "driver code", "driver num", "total points", "num races");
-printf($mask, "------------------", "------------", "----------", "------------", "---------");
+printf($mask, "------------------", "------------", "----------", "----", "------------", "---------");
+printf($mask, "driver name", "driver code", "driver num", "age", "total points", "num races");
+printf($mask, "------------------", "------------", "----------", "----", "------------", "---------");
 while($row = mysqli_fetch_array($result, MYSQLI_BOTH))
 {
-    printf($mask, "$row[driver_name]", "$row[driver_code]", "$row[driver_num]", "$row[total_points]", "$row[num_races]");
+    printf($mask, "$row[driver_name]", "$row[driver_code]", "$row[driver_num]", "$row[driver_age]", "$row[total_points]", "$row[num_races]");
 }
-printf($mask, "------------------", "------------", "----------", "------------", "---------");
+printf($mask, "------------------", "------------", "----------", "----", "------------", "---------");
 print "</pre>";
 
 mysqli_free_result($result);
